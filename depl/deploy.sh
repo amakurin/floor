76fb196b6a2fe6ecfbaff0fe3872f60135178edd
snoopnstalk <alexey@makurin.biz>
2014-05-25 23:58:01 +0400

    deployment

--- depl/deploy.sh
+++ depl/deploy.sh
@@ -1,5 +1,6 @@
 lein cljsbuild once dev2
 lein cljsbuild once prod
+java -jar depl/closure-stylesheets.jar --allowed-non-standard-function progid:DXImageTransform.Microsoft.Alpha --output-file resources/public/css/prod.css resources/public/css/base.css resources/public/css/layout.css resources/public/css/skeleton.css
 lein uberjar
 sudo service floor restart
 sudo service nginx restart
