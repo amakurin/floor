0c471fb14ef098ea1ce953de79445a68ea60fde1
Alexey Makurin <rufige@yandex.ru>
2014-05-29 21:59:12 +0400

    Fixes #12

--- depl/deploy.sh
+++ depl/deploy.sh
@@ -1,3 +1,12 @@
+cd /home/floor/caterpillar
+git pull origin
+sudo service caterpillar restart
+cd /home/floor/floor16
+git pull origin
+rm -R target/*
+rm -R resources/public/js/out/*
+rm resources/public/js/site2.js
+rm resources/public/js/site-prod.js
 lein cljsbuild once dev2
 lein cljsbuild once prod
 java -jar depl/closure-stylesheets.jar --allowed-non-standard-function progid:DXImageTransform.Microsoft.Alpha --output-file resources/public/css/prod.css resources/public/css/base.css resources/public/css/layout.css resources/public/css/skeleton.css
